10cfa23f88a97ed3f49a08989267b5afd12a6deb
myattaw <39500954+myattaw@users.noreply.github.com>
2026-01-15 16:14:26 -0500

    Initial Commit.

---
+++ src/main/java/com/hymines/currency/HyCurrencyPlugin.java
@@ -0,0 +1,21 @@
+package com.hymines.currency;
+
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+import javax.annotation.Nonnull;
+
+public class HyCurrencyPlugin extends JavaPlugin {
+
+    public HyCurrencyPlugin(@Nonnull JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    protected void setup() {
+        super.setup();
+    }
+
+
+
+}
